0d63f13655cde3ae4eb28d703a5f446c66da0c7e
Dimas Saputra <dimas.maulanads@gmail.com>
2022-08-01 14:02:01 +0700

    add logout to ContactApp

--- contacts-app/src/components/ContactApp.js
+++ contacts-app/src/components/ContactApp.js
@@ -17,6 +17,7 @@ class ContactApp extends React.Component {
     };
 
     this.onLoginSuccess = this.onLoginSuccess.bind(this);
+    this.onLogout = this.onLogout.bind(this);
   }
 
   async componentDidMount() {
@@ -39,6 +40,16 @@ class ContactApp extends React.Component {
         authedUser: data,
       };
     });
+  }
+
+  onLogout() {
+    this.setState(() => {
+      return {
+        authedUser: null
+      }
+    });
+
+    putAccessToken('');
   }
   
   render() {
@@ -66,7 +77,7 @@ class ContactApp extends React.Component {
       <div className="contact-app">
         <header className='contact-app__header'>
           <h1>Aplikasi Kontak</h1>
-          <Navigation />
+          <Navigation logout={this.onLogout} name={this.state.authedUser.name} />
         </header>
         <main>
           <Routes>
