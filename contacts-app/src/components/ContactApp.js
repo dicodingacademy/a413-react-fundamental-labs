9bbb49207bf5675a119c97f70fe13389cbd07f1c
Dimas Saputra <dimas.maulanads@gmail.com>
2022-08-01 13:04:08 +0700

    change ContactApp to class component

--- contacts-app/src/components/ContactApp.js
+++ contacts-app/src/components/ContactApp.js
@@ -5,21 +5,23 @@ import HomePage from '../pages/HomePage';
 import AddPage from '../pages/AddPage';
 
 
-function ContactApp() {
-  return (
-    <div className="contact-app">
-      <header className='contact-app__header'>
-        <h1>Aplikasi Kontak</h1>
-        <Navigation />
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/add" element={<AddPage />} />
-        </Routes>
-      </main>
-    </div>
-  );
+class ContactApp extends React.Component {
+  render() {
+    return (
+      <div className="contact-app">
+        <header className='contact-app__header'>
+          <h1>Aplikasi Kontak</h1>
+          <Navigation />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/add" element={<AddPage />} />
+          </Routes>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default ContactApp;
