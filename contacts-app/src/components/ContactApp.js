84a80e23adea0354bf9d5e0582e2621dea2d7178
Dimas Saputra <dimas.maulanads@gmail.com>
2022-08-01 13:15:47 +0700

    use regsiter page in contact app

--- contacts-app/src/components/ContactApp.js
+++ contacts-app/src/components/ContactApp.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Navigation from './Navigation';
 import HomePage from '../pages/HomePage';
 import AddPage from '../pages/AddPage';
+import RegisterPage from '../pages/RegisterPage';
 
 
 class ContactApp extends React.Component {
@@ -24,7 +25,7 @@ class ContactApp extends React.Component {
           <main>
             <Routes>
               <Route path="/*" element={<p>Halaman Login</p>} />
-              <Route path="/register" element={<p>Halaman Register</p>} />
+              <Route path="/register" element={<RegisterPage />} />
             </Routes>
           </main>
         </div>
