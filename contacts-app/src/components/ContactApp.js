bbc1347938c9a4234513b1c79d973ef8254bdae2
Dimas Saputra <dimas.maulanads@gmail.com>
2022-08-01 14:25:59 +0700

    add locale state

--- contacts-app/src/components/ContactApp.js
+++ contacts-app/src/components/ContactApp.js
@@ -14,6 +14,19 @@ class ContactApp extends React.Component {
     this.state = {
       authedUser: null,
       initializing: true,
+      localeContext: {
+        locale: 'id',
+        toggleLocale: () => {
+          this.setState((prevState) => {
+            return {
+              localeContext: {
+                ...prevState.localeContext,
+                locale: prevState.localeContext.locale === 'id' ? 'en' : 'id'
+              }
+            }
+          })
+        }
+      }
     };
 
     this.onLoginSuccess = this.onLoginSuccess.bind(this);
